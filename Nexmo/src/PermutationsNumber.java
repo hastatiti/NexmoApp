03a37b585505ac4c221538d8d13cc462bdfe1c7d
ADY <devrimytr@yahoo.com>
2019-01-12 23:57:34 +0000

    Add method permutations

---
+++ Nexmo/src/PermutationsNumber.java
@@ -0,0 +1,10 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class PermutationsNumber {
+	Set<Integer> mySet = new HashSet<Integer>();
+	public  int solution(int N) {
+		return N;
+		
+	}
+}
